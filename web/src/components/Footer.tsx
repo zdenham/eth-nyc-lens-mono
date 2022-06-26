e7e342aad0175af682086951777e42366e6bd0f8
Shandy Sulen <shandysulen@gmail.com>
2022-06-26 08:19:49 -0400

    make footer mobile-responsive

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,12 +1,14 @@
-import { HStack, Image, Link, Text, VStack } from '@chakra-ui/react';
+import { Flex, HStack, Image, Link, Text } from '@chakra-ui/react';
 import React from 'react';
 import { footerHeight, pagePaddingX, zIndexes } from '../constants';
 
 export const Footer = () => (
-    <HStack
+    <Flex
+        flexDir={{ base: 'column', md: 'row' }}
         display='flex'
         position='fixed'
-        justifyContent='space-between'
+        justifyContent={{ base: 'center', md: 'space-between' }}
+        alignItems={{ base: 'center', md: 'auto' }}
         bottom='0'
         w='100vw'
         px={pagePaddingX}
@@ -16,18 +18,16 @@ export const Footer = () => (
         bg='gray.50'
         shadow='all'
     >
-        <VStack spacing='0' alignItems='flex-start'>
-            <Text as='small' fontSize='10px'>Don’t have a&nbsp;
-                <Image display='inline' verticalAlign='middle' src='/lens.svg' boxSize='32px' /> handle?&nbsp;
-                <Link display='inline' href='https://claim.lens.xyz' textDecor='underline'>Claim one here!</Link>
-            </Text>
-        </VStack>
-        <HStack justifyContent='flex-end' spacing='4px'>
+        <Text as='small' fontSize='10px'>Don’t have a&nbsp;
+            <Image display='inline' verticalAlign='middle' src='/lens.svg' boxSize='32px' /> handle?&nbsp;
+            <Link display='inline' href='https://claim.lens.xyz' textDecor='underline'>Claim one here!</Link>
+        </Text>
+        <HStack alignItems='center' spacing='4px' fontSize='10px' mt='6px'>
             <Text>Built for</Text>
             <Link href='https://nyc.ethglobal.com/' textDecor='underline' target='_blank'>ETHNewYork 2022</Link>
-            <Link href='https://github.com/zdenham/eth-nyc-lens-mono' target='_blank' pl='8px'>
+            <Link href='https://github.com/zdenham/eth-nyc-lens-mono' target='_blank' pl='8px' pb='4px'>
                 <Image src='/github.svg' boxSize='18px' />
             </Link>
         </HStack>
-    </HStack>
+    </Flex>
 );
