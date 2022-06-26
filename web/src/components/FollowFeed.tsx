d5915d47264529fc76d2b97d4f47e54962ead365
zdenham <zdenham1@gmail.com>
2022-06-25 23:58:35 -0400

    fix linter

--- web/src/components/FollowFeed.tsx
+++ web/src/components/FollowFeed.tsx
@@ -1,15 +1,4 @@
-import {
-    Box,
-    BoxProps,
-    Button,
-    Heading,
-    HStack,
-    Image,
-    Link,
-    OrderedList,
-    Text,
-    VStack,
-} from '@chakra-ui/react';
+import { Box, BoxProps, Button, Heading, HStack, Image, Link, OrderedList, Text, VStack } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import React from 'react';
 import { useFollowing } from '../utils/lens';
@@ -27,31 +16,31 @@ const FeedItem = ({ imageUrl, name, bio, handle }) => {
 
     return (
         <Button
-            bg="white"
-            p="0"
-            height="auto"
-            w="100%"
-            justifyContent="flex-start"
-            rounded="0"
+            bg='white'
+            p='0'
+            height='auto'
+            w='100%'
+            justifyContent='flex-start'
+            rounded='0'
             _hover={{ bg: 'gray.100' }}
-            borderTop="2px solid #ECECEC"
+            borderTop='2px solid #ECECEC'
             onClick={() => push(`https://lenster.xyz/u/${handle}`)}
         >
-            <HStack alignItems="flex-start" px="20px" py="16px">
-                <Image src={imageUrl} w="60px" h="60px" />
-                <VStack alignItems="flex-start" spacing="0" fontWeight={400}>
+            <HStack alignItems='flex-start' px='20px' py='16px'>
+                <Image src={imageUrl} w='60px' h='60px' />
+                <VStack alignItems='flex-start' spacing='0' fontWeight={400}>
                     <HStack>
-                        <Text as="span" fontWeight="bold">
+                        <Text as='span' fontWeight='bold'>
                             {name}
                         </Text>
-                        <Text as="span">{handle}</Text>
+                        <Text as='span'>{handle}</Text>
                     </HStack>
-                    <Text fontSize="14px" pt="4px">
+                    <Text fontSize='14px' pt='4px'>
                         {bio}
                     </Text>
-                    <HStack pt="8px">
-                        <Image src="/clock.svg" boxSize="16px" />
-                        <Text as="span" color="gray.400" fontSize="12px">
+                    <HStack pt='8px'>
+                        <Image src='/clock.svg' boxSize='16px' />
+                        <Text as='span' color='gray.400' fontSize='12px'>
                             Followed 5 mins ago
                         </Text>
                     </HStack>
@@ -67,31 +56,24 @@ export const FollowFeed: React.FC<BoxProps> = (props) => {
     console.log('FOLLOWING: ', following);
 
     return (
-        <Box pt="40px" {...props}>
-            <Box px="20px" shadow="lg">
-                <Heading as="h2" fontSize="16px">
+        <Box pt='40px' {...props}>
+            <Box px='20px' shadow='lg'>
+                <Heading as='h2' fontSize='16px'>
                     Follow Feed
                 </Heading>
                 <Text>
-                    Look through all the friends you made through Flourish.
-                    Interact with a profile to visit their{' '}
-                    <Link
-                        href="https://lenster.xyz"
-                        target="_blank"
-                        textDecor="underline"
-                    >
+                    Look through all the friends you made through Flourish. Interact with a profile to visit their{' '}
+                    <Link href='https://lenster.xyz' target='_blank' textDecor='underline'>
                         Lenster
                     </Link>{' '}
                     profile.
                 </Text>
-                <HStack mt="40px">
-                    <Image src="/users.svg" boxSize="16px" />
-                    <Text color="gray.400">
-                        {following.length} profiles followed via Flourish
-                    </Text>
+                <HStack mt='40px'>
+                    <Image src='/users.svg' boxSize='16px' />
+                    <Text color='gray.400'>{following.length} profiles followed via Flourish</Text>
                 </HStack>
             </Box>
-            <OrderedList ml="0">
+            <OrderedList ml='0'>
                 {following.map((feedItem) => (
                     <FeedItem key={feedItem.handle} {...feedItem} />
                 ))}
