168bb61a53731ac57067dd83da089fc0334a5a7f
zdenham <zdenham1@gmail.com>
2022-06-26 01:29:33 -0400

    trying again

--- web/src/utils/LensHub.ts
+++ web/src/utils/LensHub.ts
@@ -15,7 +15,6 @@ import {
 } from 'ethers';
 import { FunctionFragment, Result, EventFragment } from '@ethersproject/abi';
 import { Listener, Provider } from '@ethersproject/providers';
-import { TypedEventFilter, TypedEvent, TypedListener, OnEvent } from './common';
 
 export type EIP712SignatureStruct = {
     v: BigNumberish;
@@ -616,45 +615,12 @@ export interface LensHubInterface extends utils.Interface {
     getEvent(nameOrSignatureOrTopic: 'Transfer'): EventFragment;
 }
 
-export type ApprovalEvent = TypedEvent<
-    [string, string, BigNumber],
-    { owner: string; approved: string; tokenId: BigNumber }
->;
-
-export type ApprovalEventFilter = TypedEventFilter<ApprovalEvent>;
-
-export type ApprovalForAllEvent = TypedEvent<
-    [string, string, boolean],
-    { owner: string; operator: string; approved: boolean }
->;
-
-export type ApprovalForAllEventFilter = TypedEventFilter<ApprovalForAllEvent>;
-
-export type TransferEvent = TypedEvent<[string, string, BigNumber], { from: string; to: string; tokenId: BigNumber }>;
-
-export type TransferEventFilter = TypedEventFilter<TransferEvent>;
-
 export interface LensHub extends BaseContract {
     connect(signerOrProvider: Signer | Provider | string): this;
     attach(addressOrName: string): this;
     deployed(): Promise<this>;
 
     interface: LensHubInterface;
-
-    queryFilter<TEvent extends TypedEvent>(
-        event: TypedEventFilter<TEvent>,
-        fromBlockOrBlockhash?: string | number | undefined,
-        toBlock?: string | number | undefined
-    ): Promise<Array<TEvent>>;
-
-    listeners<TEvent extends TypedEvent>(eventFilter?: TypedEventFilter<TEvent>): Array<TypedListener<TEvent>>;
-    listeners(eventName?: string): Array<Listener>;
-    removeAllListeners<TEvent extends TypedEvent>(eventFilter: TypedEventFilter<TEvent>): this;
-    removeAllListeners(eventName?: string): this;
-    off: OnEvent<this>;
-    on: OnEvent<this>;
-    once: OnEvent<this>;
-    removeListener: OnEvent<this>;
 
     functions: {
         approve(
@@ -1519,29 +1485,6 @@ export interface LensHub extends BaseContract {
         whitelistProfileCreator(profileCreator: string, whitelist: boolean, overrides?: CallOverrides): Promise<void>;
 
         whitelistReferenceModule(referenceModule: string, whitelist: boolean, overrides?: CallOverrides): Promise<void>;
-    };
-
-    filters: {
-        'Approval(address,address,uint256)'(
-            owner?: string | null,
-            approved?: string | null,
-            tokenId?: BigNumberish | null
-        ): ApprovalEventFilter;
-        Approval(owner?: string | null, approved?: string | null, tokenId?: BigNumberish | null): ApprovalEventFilter;
-
-        'ApprovalForAll(address,address,bool)'(
-            owner?: string | null,
-            operator?: string | null,
-            approved?: null
-        ): ApprovalForAllEventFilter;
-        ApprovalForAll(owner?: string | null, operator?: string | null, approved?: null): ApprovalForAllEventFilter;
-
-        'Transfer(address,address,uint256)'(
-            from?: string | null,
-            to?: string | null,
-            tokenId?: BigNumberish | null
-        ): TransferEventFilter;
-        Transfer(from?: string | null, to?: string | null, tokenId?: BigNumberish | null): TransferEventFilter;
     };
 
     estimateGas: {
